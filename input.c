fef85401278dae38d413125d7c4143d8dc1e0de7
kawawarika <k.rikatakapon@gmail.com>
2024-10-09 14:33:23 +0900

    合計なくした

--- input.c
+++ input.c
@@ -13,17 +13,16 @@ void input(){
         printf("\n[NO.%d]\n\n",num+1);
 
         gets(garbage[num]);
-        printf("国名："); gets(country[num]); trim(country[num]);
+        printf("国名："); gets(country[num]); 
         printf("金："); scanf("%d", &gold[num]);
         printf("銀："); scanf("%d", &silver[num]);
         printf("銅："); scanf("%d", &bronze[num]);
-        printf("合計："); scanf("%d", &total[num]);
-        printf("%s, %d, %d, %d, %d",country,gold,silver,bronze,total);
+        printf("%s, %d, %d, %d ",country,gold,silver,bronze);
 
         num++;
         printf("\n 続けて入力しますか？(y/n)");
         scanf("%*c%c", &c);
-    }while (c == 'y '|| c == 'Y');
+    }while (c == 'y'|| c == 'Y');
 
   
 
