4291760d8776e88f3930c52958c9de7a8c8f24bb
kawawarika <k.rikatakapon@gmail.com>
2024-10-09 14:09:35 +0900

    名前変更

--- input.c
+++ input.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
-void load();
 
-int main(void){
-    load();
+
+void input(){
     
-}
-
-void load(){
-    FILE *fp;
-    char *fname = "load.cvs";
-    char country[30][100];
-    int gold[100], silver[100], bronze[100], total[100];
-    char garbage[100][100];
-
     int i = 0;
     int num = 0;
     char c;
@@ -35,18 +25,7 @@ void load(){
         scanf("%*c%c", &c);
     }while (c == 'y '|| c == 'Y');
 
-    fp=fopen(fname, "w");
-    if (fp == NULL){
-        printf("ファイルが開けません\n");
-        return -1;
-    }
-    for (i = 0; i < num; i++){
-        fprintf(fp, "%s, %d, %d, %d, %d\n", country[i], gold[i], silver[i], bronze[i], total[i]);
-    }
-
-    fclose(fp);
-    printf("---output---\n");
-    return 0;
+  
 
 
 
