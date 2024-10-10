0aaf1217fdad2f172e8d281d6b66e42de31e9386
kazu-321 <kzs321kzs@gmail.com>
2024-10-10 14:36:41 +0900

    [update] input.cフォーマット変更

--- input.c
+++ input.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 void input(){
-    printf("\n[NO.%d]\n\n",data_size+1);
+    printf("[NO.%d]\n",data_size+1);
     printf("国名："); scanf("%s",country[data_size]); 
     printf("金："); scanf("%d", &gold[data_size]);
     printf("銀："); scanf("%d", &silver[data_size]);
     printf("銅："); scanf("%d", &bronze[data_size]);
-    printf("%s, %d, %d, %d \n",country[data_size],gold[data_size],silver[data_size],bronze[data_size]);
     data_size++;
 }
