646dc583c8424fcba332ece36df2af239b95a127
kazu-321 <kzs321kzs@gmail.com>
2024-10-16 13:47:34 +0900

    [update] help更新& show引数月

--- main.c
+++ main.c
@@ -23,15 +23,16 @@ int main(int argc, char* argv[]){
         printd("input command: %s\n",command);
         if(strcmp(command,"help")==0){
             printd("show help\n");
-            printf("+-----------------------------------+\n");
-            printf("| コマンド | 説明                    |\n");
-            printf("+----------+-------------------------+\n");
-            printf("| help     | ヘルプを表示します      |\n");
-            printf("| input    | データを追加します      |\n");
-            printf("| sort     | データをソートします    |\n");
-            printf("| show     | データ一覧を表示します  |\n");
-            printf("| exit     | プログラムを終了します  |\n");
-            printf("+------------------------------------+\n");
+            printf("+-------------------------------------+\n");
+            printf("| コマンド | 説明                      |\n");
+            printf("+----------+---------------------------+\n");
+            printf("| help     | ヘルプを表示します        |\n");
+            printf("| input    | データを追加します        |\n");
+            printf("| sort     | データをソートします      |\n");
+            printf("| show     | データ一覧を表示します(-h)|\n");
+            printf("| search   | データを検索します        |\n");
+            printf("| exit     | プログラムを終了します    |\n");
+            printf("+--------------------------------------+\n");
         }else if(strcmp(command,"input")==0){
             printd("input data start\n");
             input();
@@ -47,6 +48,15 @@ int main(int argc, char* argv[]){
             printf("ソート完了 (モード: %d)\n",mode);
         }else if(strcmp(command,"show")==0){
             show();
+        }else if(strcmp(command,"show -h")==0){
+            printf("+---------------------------------------+\n");
+            printf("|    引数     |   説明                  |\n");
+            printf("+-------------+-------------------------+\n");
+            printf("| -h          | ヘルプを表示します      |\n");
+            printf("| --medalrank | メダルランクを表示します|\n");
+            printf("+---------------------------------------+\n");
+        }else if(strcmp(command,"show --medalrank")==0){
+            show_medalrank();
         }else if(strcmp(command,"exit")==0){
             printd("プログラムを終了します\n");
             break;
