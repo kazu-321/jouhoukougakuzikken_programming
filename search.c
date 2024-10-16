2968045330ef3abb3b1a031b4e448546a2b3dcd8
len-0202 <06renkon@gmail.com>
2024-10-16 15:01:41 +0900

    search関数にprintd追加

--- search.c
+++ search.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 int search(){
+    printd("search\n");
     char* name;
 
     printf("国名を入力 >>>"); scanf("%s",name);
@@ -8,14 +9,16 @@ int search(){
     for(int i=0;i<=data_size;i++){
         if(strcmp(name,country[i])==0){
             printf("国名：%s\n",country[i]);
-            printf("メダル順位：%d位",medal_rank[i]);
-            printf("金：%d個\n",gold[i]);
-            printf("銀：%d個\n",silver[i]);
-            printf("銅：%d個\n",bronze[i]);
+            printf("メダル順位：%2d位",medal_rank[i]);
+            printf("金：%2d個\n",gold[i]);
+            printf("銀：%2d個\n",silver[i]);
+            printf("銅：%2d個\n",bronze[i]);
 
             break;
         }
+        printd("if\n");
     }
+    printd("for");
     
     return 0;
 
