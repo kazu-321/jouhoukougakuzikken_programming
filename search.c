07151a7f64648e4482464ceba011225c73f2244a
len-0202 <06renkon@gmail.com>
2024-10-23 13:22:38 +0900

    デバッグ用printd入力

--- search.c
+++ search.c
@@ -5,7 +5,9 @@ void search(){
 
     printf("国名を入力 >>>"); scanf("%s",name);
     
+    printd("for");
     for(int i=0;i<=data_size;i++){
+        printd("if")
         if(strcmp(name,data[i].country)==0){
             printf("国名：%s\n",data[i].country);
             printf("メダル順位：%2d位",data[i].medal_rank);
